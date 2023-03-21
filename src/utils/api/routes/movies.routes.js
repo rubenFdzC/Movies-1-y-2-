e88c4fd404f93d75a9bb217195f54ed9f77a449a
rubenFdzC <rubenfdz1998@gmail.com>
2023-03-21 23:33:42 +0100

    cambiamos el .env para no subir las variables sensibles

--- src/utils/api/routes/movies.routes.js
+++ src/utils/api/routes/movies.routes.js
@@ -3,12 +3,11 @@ const express = require('express');
 const routerMovies = express.Router(); // enrutador para conectar con el endpoint
 
 
-const {getMovies,getMoviesById,postMovies,/*sendMovies*/deleteMovies,putMovie,getMovieByYear} = require('../controllers/movies.controllers');
+const {getMovies,getMoviesById,postMovies,deleteMovies,putMovie,getMovieByYear} = require('../controllers/movies.controllers');
 routerMovies.get('/',getMovies )
 routerMovies.get('/movies/year/:year', getMovieByYear)
 routerMovies.get('/movies/:id',getMoviesById)
 routerMovies.post('/',postMovies )
-//routerMovies.send('/',sendMovies )
 routerMovies.delete('/',deleteMovies )
 routerMovies.put('/',putMovie )
 
